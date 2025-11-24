91c2e04a542816c02f32d566394a323e4304d69f
João Victor <joaovictorsalotialves12@gmail.com>
2025-11-24 15:51:44 -0300

    Uso do Where

---
+++ consultas_avancadas/uso_do_where.sql
@@ -0,0 +1,7 @@
+SELECT * FROM customers;
+
+SELECT * FROM customers WHERE first_name = 'Pedro';
+SELECT * FROM customers WHERE email = 'william.passos@exemplo.com';
+SELECT * FROM customers WHERE city = 'Porto Alegre';
+SELECT * FROM customers WHERE customer_id = 44;
+SELECT * FROM customers WHERE created_at = '2024-11-28 00:00:00';
