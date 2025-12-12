b18194b21e81fccd137bcee190edb5f05edc790b
João Victor <joaovictorsalotialves12@gmail.com>
2025-12-12 14:13:17 -0300

    Índices e Árvores B-Tree vs. Hash vs. GiST vs. GIN

--- otimizacao_e_performance/indices_e_arvores_b-tree_vs_hash_vs_gist_vs_gin.sql
+++ otimizacao_e_performance/indices_e_arvores_b-tree_vs_hash_vs_gist_vs_gin.sql
@@ -12,3 +12,11 @@ ALTER TABLE products
 UPDATE products 
   SET tags = ARRAY['promo', 'novo'] 
   WHERE product_id % 5 = 0; -- Atualizando alguns produtos com tags
+
+CREATE INDEX idx_products_tags_gin 
+  ON products USING GIN(tags); -- Criando um índice GIN na coluna tags
+
+EXPLAIN ANALYZE
+SELECT *
+FROM products
+WHERE tags @> ARRAY['promo']; -- Analisando a consulta com índice GIN
