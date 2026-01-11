548671f0f10ffc54f3e5f84689f27c9be2b8e21a
João Victor <joaovictorsalotialves12@gmail.com>
2026-01-11 14:20:50 -0300

    Stored Procedure e Transações

---
+++ projeto_final/stored_procedure_e_transacoes.sql
@@ -0,0 +1,30 @@
+CREATE PROCEDURE sp_create_loan(
+  p_copy_id   INT,
+  p_member_id INT,
+  p_days      INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  INSERT INTO loans(copy_id, member_id, loan_date, due_date)
+  VALUES (p_copy_id, p_member_id, NOW(), NOW() + (p_days || ' days')::interval);
+
+  UPDATE book_copies
+  SET status = 'loaned'
+  WHERE copy_id = p_copy_id;
+END;
+$$;
+
+CALL sp_create_loan(124, 46, 14);
+
+
+DO $$
+  BEGIN
+    INSERT INTO loans(copy_id, member_id, loan_date, due_date)
+    VALUES (198, 11, NOW(), NOW() + INTERVAL '14 days');
+  
+    UPDATE book_copies
+    SET status = 'loaned'
+    WHERE copy_id = 198;
+  END
+$$;
